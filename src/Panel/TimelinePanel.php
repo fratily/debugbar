ba55ec9663ffe1c9d79026be8f72648757d609b0
kento-oka <kento.oka@kentoka.com>
2018-06-01 13:34:26 +0900

    パネルスタイル追加・タイムライン表示用の各種クラス追加

---
+++ src/Panel/TimelinePanel.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * FratilyPHP Debug
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author      Kento Oka <kento.oka@kentoka.com>
+ * @copyright   (c) Kento Oka
+ * @license     MIT
+ * @since       1.0.0
+ */
+namespace Fratily\Debug\Panel;
+
+class TimelinePanel extends AbstractPanel{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function normalize(array $data){
+        if(!isset($data["times"]) || !is_array($data["times"])){
+            $data["times"]   = [];
+        }
+
+        return $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTplName(){
+        return "panel/timeline.twig";
+    }
+}
